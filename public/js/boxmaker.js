c48533767646a40ace184025415f2f2dd39dd356
PCELUVNLAPTOP\Akhil <akhil.ramlakan@gmail.com>
2016-08-28 21:18:08 -0400

    now renders default boxes (without label)!

--- public/js/boxmaker.js
+++ public/js/boxmaker.js
@@ -1,4 +1,118 @@
 $(function () {
+    $("#box-form").change(function () {
+        var text = $("#box-text").val();
+        var label = $("#box-label").val();
+        var spacing = $("#box-spacing").val();
+        var offset = $("#box-offset").val();
+        
+        $("#box").val(boxify(text, label, spacing, offset));
+    });
     
+    function boxify (text, label, spacing, offset) {
+        var box = "";
+        
+        if (!spacing) {
+            var spacing = 2;
+        }
+        
+        if (!offset) {
+            var offset = 10;
+        }
+        
+        for (var i = text.length - 1; i > 0; i--) {
+            box += spacer(offset);
+            box += spacer(i);
+            
+            if (i+1 === text.length) {
+                box += spaceText(text, spacing, true);
+            } else {
+                box += text.substring(i, i+1);
+                box += spacer((text.length * (spacing + 1) - (spacing + 2)));
+                box += text.substring(text.length - (i+1), text.length - i);
+                box += spacer(text.length - i - 2);
+                box += text.substring(text.length - (i+1), text.length - i);
+            }
+            
+            if (i === 1) {
+                box += "\n";
+                box += spacer(offset);
+                box += spaceText(text, spacing, false);
+                box += spacer(text.length - 2);
+                box += text.substring(text.length - i, text.length);
+            }
+            
+            box += "\n";
+        }
+        
+        for (var i = text.length - 1; i > 1; i--) {
+            box += spacer(offset);
+            box += text.substring(text.length - i, text.length - i + 1);
+            
+            if (text.length % 2 === 0) {
+                box += spacer(text.length * spacing + 2);
+            } else {
+                box += spacer(text.length * spacing + 3);
+            }
+            
+            box += text.substring(i - 1, i);
+            box += spacer(text.length - 2 - (text.length - i));
+            
+            if (i > 1) {
+                box += text.substring(i - 1, i);
+            }
+            
+            box += "\n";
+        }
+        
+        box += spacer(offset);
+        box += spaceText(text, spacing, true);
+        
+        console.log(box);
+        
+        return box;
+    }
     
+    function spacer (numSpaces, character) {
+        var spaceString = "";
+        
+        if (!character) {
+            var character = " ";
+        }
+        
+        for (var i = 0; i < numSpaces; i++) {
+            spaceString += character;
+        } 
+        
+        return spaceString;
+    }
+    
+    function spaceText (text, spacing, reverse, character) {
+        var spacedText = "";
+        
+        if (!character) {
+            var character = "";
+        }
+        
+        if (!reverse) {
+            for (var i = 0; i < text.length; i++) {
+                
+                spacedText += text.substring(i, i+1);
+                
+                if (i < text.length - 1) {
+                    spacedText += spacer(spacing, character);   
+                }
+            }
+        } else {
+            for (var i = text.length; i > 0; i--) {
+                
+                spacedText += text.substring(i-1, i);
+                
+                if (i > 1) {
+                    spacedText += spacer(spacing, character);   
+                }
+            }
+        }
+        
+        return spacedText;
+    }
 });
